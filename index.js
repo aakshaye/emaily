985b531e0ef53240209fbe6ef49805167200f109
Aakshaye <aakshaye@yahoo.co.in>
2019-06-24 15:43:15 -0400

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); // returns new express app
 
 app.get('/', (req, res) => {
-    res.send({'hi': 'there'});
+    res.send({'hello': 'there'});
 });
 
 const PORT = process.env.PORT || 5000;
