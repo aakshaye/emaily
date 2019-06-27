11e16adaf2d1ed28333f271c6569c048bbda6b6b
Aakshaye <aakshaye@yahoo.co.in>
2019-06-27 18:11:57 -0400

    changed config keys

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === 'process.env.NODE_ENV' == 'production') {
+    // return prod set of keys
+    module.exports = require('./prod');
+} else {
+    // we are in development - return dev keys
+    module.exports = require('./dev');
+}
---
+++ config/prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+}
+// prod 5neWkJwVKdj9Grxl
+    // mongodb+srv://ag5308:5neWkJwVKdj9Grxl@cluster0-udhop.mongodb.net/test?retryWrites=true&w=majority
+
+    // prod clientid = 632095785853-v92t3pcl87cbsfoe9nlu6eb8pk1k2asf.apps.googleusercontent.com
+    // prod client secret = 5WJ7XUQpS_4eXrb8JYC0nHZF 
