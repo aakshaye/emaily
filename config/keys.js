145258620ada5e4046fa9b2c4afe368f4f281468
Aakshaye <aakshaye@yahoo.co.in>
2019-06-27 18:15:21 -0400

    typo in keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'process.env.NODE_ENV' == 'production') {
+if (process.env.NODE_ENV === 'production') {
     // return prod set of keys
     module.exports = require('./prod');
 } else {
