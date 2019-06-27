d74fe7efb2355c9b186d3ef2e05560fd8599a8f5
Aakshaye <aakshaye@yahoo.co.in>
2019-06-27 19:03:05 -0400

    debug log

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     // return prod set of keys
+    console.log('here');
     module.exports = require('./prod');
 } else {
     // we are in development - return dev keys
