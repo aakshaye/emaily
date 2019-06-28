81c207f52296c4f164a224dc238a77a9eec28503
Aakshaye <aakshaye@yahoo.co.in>
2019-06-27 22:22:35 -0400

    minor change

--- config/prod.js
+++ config/prod.js
@@ -4,8 +4,3 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 }
-// prod 5neWkJwVKdj9Grxl
-    // mongodb+srv://ag5308:5neWkJwVKdj9Grxl@cluster0-udhop.mongodb.net/test?retryWrites=true&w=majority
-
-    // prod clientid = 632095785853-v92t3pcl87cbsfoe9nlu6eb8pk1k2asf.apps.googleusercontent.com
-    // prod client secret = 5WJ7XUQpS_4eXrb8JYC0nHZF 
